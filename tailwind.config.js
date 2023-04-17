54afaad2a4ec3ac82a6bd9bdacf557c4166e43ee
Shaun Parker <mail@shaunparker.me>
2023-04-18 00:16:48 +0100

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['templates/**/*.twig'],
+    theme: {
+        extend: {},
+      },
+      plugins: [
+        {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      ],
+  }
